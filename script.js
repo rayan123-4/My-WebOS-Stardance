2eae372bc707beba103405a11298351d4d8ac5a8
rayan123-4 <Rayan.m.alsharaf@gmail.com>
2026-09-19 00:06:07 +1200

    making progress on the snake game, it works fine its just a few bugs wiht the layout and actual window, also maybe some variable name mix ups

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
   
 // Code for making the DIV element draggable:
-document.querySelectorAll(".window-box, .gallery-window, .roar-window, .tiger-map-window, .settings-window, .Tiger-Information, .Weather-Window, .real-weather-window, manual-window").forEach(dragElement);
+document.querySelectorAll(".window-box, .gallery-window, .roar-window, .tiger-map-window, .settings-window, .Tiger-Information, .Weather-Window, .real-weather-window, manual-window, .snake-Game-Window").forEach(dragElement);
 
 // Function for making the window drag:
 function dragElement(element) {
@@ -145,7 +145,7 @@ function handleIconTap(element) {
 }
 
 // For drag on gallery window:
-dragElement(document.querySelector("#tigergallery"));
+dragElement(document.querySelector("#gallerywindow"));
 
 var galleryWindow = document.querySelector("#gallerywindow");
 var galleryIcon = document.querySelector("#tigergallery");
@@ -160,6 +160,25 @@ if (galleryIcon) {
 if (galleryWindowClose) {
   galleryWindowClose.addEventListener("click", function () {
     closeWindow(galleryWindow);
+  });
+}
+
+// For drag on Tiger Snake game window:
+dragElement(document.querySelector("#snakeGameWindow"));
+
+var snakeWindow = document.querySelector("#snakeGameWindow");
+var snakeIcon = document.querySelector("#snakeIcon");
+var snakeWindowClose = document.querySelector("#SnakeWindow-close");
+
+if (snakeIcon) {
+  snakeIcon.addEventListener("click", function () {
+    openWindow(snakeWindow);
+  });
+}
+
+if (snakeWindowClose) {
+  snakeWindowClose.addEventListener("click", function () {
+    closeWindow(snakeWindow);
   });
 }
 
@@ -570,3 +589,194 @@ searchBox.addEventListener("keydown", (event) => {
 
     });
   }
+
+// Making the Tiger Snake game:
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#gameResetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "green";
+const snakeColor = "orange";
+const snakeBorder = "black";
+const foodColor = "red";
+const unitSize = 25;
+
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+
+// Setting snake positon
+let snake = [
+  {x:unitSize * 4, y:0},
+  {x:unitSize * 3, y:0},
+  {x:unitSize * 2, y:0},
+  {x:unitSize, y:0},
+  {x:0, y:0}
+];
+
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
+
+gameStart();
+
+function gameStart(){
+  running = true;
+  scoreText.textContent = score;
+  createFood();
+  drawFood();
+  nextTick();
+};
+
+function nextTick(){
+  if(running) {
+    setTimeout(() => {
+      clearBoard();
+        drawFood();
+        moveSnake();
+        drawSnake();
+        checkGameOver();
+        nextTick();
+    }, 125)
+  }
+  else {
+    displayGameOver();
+  }
+};
+
+function clearBoard(){
+  ctx.fillStyle = boardBackground;
+  ctx.fillRect(0, 0, gameWidth, gameHeight);
+};
+
+function createFood(){
+  function randomFood(min, max){
+    const randNum = Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
+    return randNum;
+  }
+  foodX = randomFood(0,gameWidth - unitSize);
+  foodY = randomFood(0,gameWidth - unitSize);
+};
+
+function drawFood(){
+  ctx.fillStyle = foodColor;
+  ctx.fillRect(foodX, foodY, unitSize, unitSize);
+};
+
+function moveSnake(){
+  const snakeHead = {x: snake[0].x + xVelocity,
+    y: snake[0].y + yVelocity};
+
+    snake.unshift(snakeHead);
+    // If food is eaten:
+    if(snake[0].x === foodX && snake[0].y === foodY){
+      score+=1;
+      scoreText.textContent = score;
+      createFood();
+    }
+    else{
+      snake.pop();
+    }
+};
+
+function drawSnake(){
+  ctx.fillStyle = snakeColor;
+  ctx.strokeStyle = snakeBorder;
+  snake.forEach(snakePart => {
+    ctx.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);
+    ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
+  })
+};
+
+function changeDirection(event){
+  const gameKeyPressed = event.keyCode;
+  const LEFTkey = 37;
+  const UPkey = 38;
+  const RIGHTkey = 39;
+  const DOWNkey = 40;
+
+  const goingLeft = (xVelocity == -unitSize);
+  const goingUp = (yVelocity == -unitSize);
+  const goingRight = (xVelocity == unitSize);
+  const goingDown = (yVelocity == unitSize);
+
+
+  switch(true){
+      case(gameKeyPressed == LEFTkey && !goingRight):
+          xVelocity = -unitSize;
+          yVelocity = 0;
+          break;
+
+      case(gameKeyPressed == UPkey && !goingDown):
+          xVelocity = 0;
+          yVelocity = -unitSize;
+          break;
+
+      case(gameKeyPressed == RIGHTkey && !goingLeft):
+          xVelocity = unitSize;
+          yVelocity = 0;
+          break;
+      
+      case(gameKeyPressed == DOWNkey && !goingUp):
+          xVelocity = 0;
+          yVelocity = unitSize; 
+          break;
+  }
+
+};
+
+function checkGameOver(){
+    switch(true) {
+        case (snake[0].x < 0):
+          running = false;
+          break;
+
+        case (snake[0].x >= gameWidth):
+          running = false;
+          break;
+
+        case (snake[0].y < - 0):
+          running = false;
+          break;
+
+        case (snake[0].y >= gameHeight):
+          running = false;
+          break;
+  }
+  for(let i = 1; i < snake.length; i+=1){
+    if(snake[i].x == snake[0].x && snake[i].y == snake[0].y ){
+      running = false;
+    }
+
+  }
+
+};
+
+function displayGameOver(){
+  ctx.font = "50px MV Boli";
+  ctx.fillStyle = "black";
+  ctx.textAlign = "center";
+  ctx.fillText("GAME OVER!", gameWidth / 2, gameHeight / 2)
+  running = false;
+};
+
+function resetGame(){
+  score = 0;
+  xVelocity = unitSize;
+  yVelocity = 0;
+
+  snake = [
+  {x:unitSize * 4, y:0},
+  {x:unitSize * 3, y:0},
+  {x:unitSize * 2, y:0},
+  {x:unitSize, y:0},
+  {x:0, y:0}
+];
+gameStart();
+
+};
+
